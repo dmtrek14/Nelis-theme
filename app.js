682396d456d2465b05d9ae41c927f7873f677fd4
Danielle Mayabb <dmayabb@lcb.state.nv.us>
2019-12-26 14:44:29 -0800

    Organizing LESS

--- app.js
+++ app.js
@@ -4,25 +4,42 @@ var mainMenu = document.getElementById('mainMenu');
 var nelisMenu = document.getElementById('nelisMenu');
 var nelisMenuContents = document.getElementById('nelisMain');
 
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
 if (window.innerWidth < mqMedium) {
     mainMenu.classList.add('fixed-top');
     nelisMenu.classList.remove('sticky-top');
     nelisMenuContents.classList.add('nav-overlay');
+
 } else if (window.innerWidth >= mqMedium) {
     mainMenu.classList.remove('fixed-top');
     nelisMenu.classList.add('sticky-top');
     nelisMenuContents.classList.remove('nav-overlay')
 
-    //Shrinking sticky menu
-    $(window).scroll(function () {
-        /* affix after scrolling 100px */
-        if ($(document).scrollTop() > 100) {
-            $(nelisMenu).addClass('shrink');
-        } else {
-            $(nelisMenu).removeClass('shrink');
-        }
-    });
+
 };
+
+
+ready(() => {
+    if (window.innerWidth >= mqMedium) {
+        //Shrinking sticky menu
+        $(window).scroll(function () {
+            /* affix after scrolling 100px */
+            if ($(document).scrollTop() > 100) {
+                $(nelisMenu).addClass('shrink');
+            } else {
+                $(nelisMenu).removeClass('shrink');
+            }
+        });
+    } else {
+        if (document.querySelector('.nav-overlay').classList.contains('show')) {
+            //listen for click on elipsis and close the nav panel
+        }
+    }
+});
 
 function changeMenuClassesForMobile() {
     if (window.innerWidth < mqMedium) {
