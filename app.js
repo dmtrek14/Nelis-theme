1ac5a11c35ec845fa5ea1f2fffe1da2e38eb80c1
Danielle Mayabb <dmayabb@lcb.state.nv.us>
2019-12-26 12:09:40 -0800

    A few js and styling changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-//okay, if the viewport is a certain size or smaller, we need to add/remove classes to the menu so that it changes to be a vertical nav that will get overlayed
+//TODO: clean this the hell up
 var mqMedium = 768;
-var mqLarge = 992;
 var mainMenu = document.getElementById('mainMenu');
 var nelisMenu = document.getElementById('nelisMenu');
 var nelisMenuContents = document.getElementById('nelisMain');
@@ -13,6 +12,16 @@ if (window.innerWidth < mqMedium) {
     mainMenu.classList.remove('fixed-top');
     nelisMenu.classList.add('sticky-top');
     nelisMenuContents.classList.remove('nav-overlay')
+
+    //Shrinking sticky menu
+    $(window).scroll(function () {
+        /* affix after scrolling 100px */
+        if ($(document).scrollTop() > 100) {
+            $(nelisMenu).addClass('shrink');
+        } else {
+            $(nelisMenu).removeClass('shrink');
+        }
+    });
 };
 
 function changeMenuClassesForMobile() {
@@ -27,27 +36,5 @@ function changeMenuClassesForMobile() {
     }
 }
 
+
 window.onresize = changeMenuClassesForMobile;
-
-if (window.innerWidth >= mqMedium) {
-    //Shrinking sticky menu
-    $(window).scroll(function () {
-        /* affix after scrolling 100px */
-        if ($(document).scrollTop() > 100) {
-            $(nelisMenu).addClass('shrink');
-        } else {
-            $(nelisMenu).removeClass('shrink');
-        }
-    });
-}
-
-
-$(document).ready(function () {
-    // $('#toggleNelisMain').on('click', function () {
-    //     $('.overlay').toggleClass('invisible');
-    // });
-    // $('.overlay').on('click', function () {
-    //     $('#toggleNelisMain').click();
-    //     $('.overlay').addClass('invisible');
-    // });
-});
