5f33e6282f52c23dbf8f03cf968c8e4772931f15
Danielle Mayabb <dmayabb@lcb.state.nv.us>
2019-12-27 09:16:51 -0800

    Reworrking js, styling dev env info badges

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
-//TODO: clean this the hell up
 var mqMedium = 768;
-var mainMenu = document.getElementById('mainMenu');
-var nelisMenu = document.getElementById('nelisMenu');
-var nelisMenuContents = document.getElementById('nelisMain');
-//var buttonGroup = document.querySelector('.btn-group');
-
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-}
-
-if (window.innerWidth < mqMedium) {
-    mainMenu.classList.add('fixed-top');
-    nelisMenu.classList.remove('sticky-top');
-    nelisMenuContents.classList.add('nav-overlay');
-    //buttonGroup.classList.replace('btn-group', 'btn-group-vertical');
-} else if (window.innerWidth >= mqMedium) {
-    mainMenu.classList.remove('fixed-top');
-    nelisMenu.classList.add('sticky-top');
-    nelisMenuContents.classList.remove('nav-overlay')
-    //buttonGroup.classList.replace('btn-group-vertical', 'btn-group');
-};
+var $mainMenu = $('#mainMenu');
+var $nelisMenu = $('#nelisMenu');
+var $nelisMenuContents = $('#nelisMain');
 
 
-ready(() => {
-    if (window.innerWidth >= mqMedium) {
-        //Shrinking sticky menu
-        $(window).scroll(function () {
-            /* affix after scrolling 100px */
-            if ($(document).scrollTop() > 100) {
-                $(nelisMenu).addClass('shrink');
-            } else {
-                $(nelisMenu).removeClass('shrink');
-            }
-        });
-    } else {
-        if (document.querySelector('.nav-overlay').classList.contains('show')) {
-            //listen for click on elipsis and close the nav panel
+$(function () {
+    var $window = $(window);
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < mqMedium) {
+            $mainMenu.addClass('fixed-top');
+            $nelisMenu.removeClass('sticky-top');
+            $nelisMenuContents.addClass('nav-overlay');
+        } else {
+            $mainMenu.removeClass('fixed-top');
+            $nelisMenu.addClass('sticky-top');
+            $nelisMenuContents.removeClass('nav-overlay');
+
+            $(window).scroll(function () {
+                if ($(document).scrollTop() > 100) {
+                    $nelisMenu.addClass('shrink');
+                } else {
+                    $nelisMenu.removeClass('shrink');
+                }
+            });
         }
     }
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
+
+
+    $('#toggleNvLeg').on('click', function () {
+        if ($('.nav-overlay').hasClass('show')) {
+            $('.nav-overlay').removeClass('show');
+        }
+    });
+
 });
-
-function changeMenuClassesForMobile() {
-    if (window.innerWidth < mqMedium) {
-        mainMenu.classList.add('fixed-top');
-        nelisMenu.classList.remove('sticky-top');
-        nelisMenuContents.classList.add('nav-overlay');
-    } else if (window.innerWidth >= mqMedium) {
-        mainMenu.classList.remove('fixed-top');
-        nelisMenu.classList.add('sticky-top');
-        nelisMenuContents.classList.remove('nav-overlay')
-    }
-}
-
-
-window.onresize = changeMenuClassesForMobile;
